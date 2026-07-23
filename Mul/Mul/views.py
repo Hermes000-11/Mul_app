ad44254fb4646f50968877021a4100da8019bef4
Hermes000-11 <basirasekov@yandex.ru>
2026-07-23 23:30:29 +0300

    recreate the registration form

--- Mul/Mul/views.py
+++ Mul/Mul/views.py
@@ -4,7 +4,3 @@ from django.contrib.auth.forms import UserCreationForm
 
 def bunker(request):
     return render(request, 'home.html')
-
-def registration(request):
-    form = UserCreationForm()
-    return render(request, 'registration_form.html', {'form': form})
--- Mul/room/views.py
+++ Mul/room/views.py
@@ -3,7 +3,3 @@ from django.contrib.auth.forms import UserCreationForm
 
 def room(request, room_name):
     return render(request, 'room/chat_show.html', {'room_name': room_name})
-
-def registration(request):
-    form = UserCreationForm()
-    return render(request, 'room/registration_form.html', {'form': form})
