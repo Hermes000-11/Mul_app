f6e0f269bfed2a9b1bef4608528e14f37d0d6036
Hermes000-11 <basirasekov@yandex.ru>
2026-08-03 20:19:58 +0300

    covering

--- Mul/Mul/views.py
+++ Mul/Mul/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -6,5 +6,12 @@ def bunker(request):
     return render(request, 'home.html')
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('room:room')
+    else:
+        form = UserCreationForm()
     form = UserCreationForm()
     return render(request, 'registration_form.html', {'form': form})
--- Mul/room/views.py
+++ Mul/room/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 def room(request, room_name):
     return render(request, 'room/chat_show.html', {'room_name': room_name})
-
-def registration(request):
-    form = UserCreationForm()
-    return render(request, 'room/registration_form.html', {'form': form})
